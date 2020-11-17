997756225a265fe94f6ecdf50a3e00adc14fb7c1
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-17 19:16:47 +0700

    Fix API url typo

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const pokedexUrl = {
   coreUrl: 'https://pokeapi.co/api/v2', // @docs - https://pokeapi.co/docs/v2
-  imageUrl: 'https://pokeres.bastionbot.org' // @format -  ${url}/image/pokemon/${pokemonId}.png
+  imageUrl: 'https://pokeres.bastionbot.org' // @format -  ${url}/images/pokemon/${pokemonId}.png
 }
 
 export default pokedexUrl
