f121a62bcb144deb71e7fbbfe6c0fb044eed1093
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-17 19:16:24 +0700

    Add image url on pokemon data response

--- src/store/selectors/pokemon.js
+++ src/store/selectors/pokemon.js
@@ -6,15 +6,24 @@ import apiConfig from '../../config/api'
 export const pokemonListQuery = selector({
   key: 'pokemonListQuery',
   get: async () => {
-    const pokemonList = await axios.get(`${apiConfig.coreUrl}/pokemon`)
-    return pokemonList.data
+    // retrive pokemon list
+    const { data: pokemonList } = await axios.get(`${apiConfig.coreUrl}/pokemon`)
+    // map pokemon list to have full resolution image url
+    return pokemonList.map(pokemon => ({
+      ...pokemon,
+      _image: `${apiConfig.imageUrl}/images/pokemon/${pokemon.id}.png`
+    }))
   }
 })
 
 export const pokemonQuery = selectorFamily({
   key: 'pokemonQuery',
   get: pokemonId => async () => {
-    const pokemon = await axios.get(`${apiConfig.coreUrl}/pokemon/${pokemonId}`)
-    return pokemon.data
+    // retrive pokemon data by specific id
+    const { data: pokemon } = await axios.get(`${apiConfig.coreUrl}/pokemon/${pokemonId}`)
+    return {
+      ...pokemon,
+      _image: `${apiConfig.imageUrl}/images/pokemon/${pokemon.id}.png`
+    }
   }
 })
