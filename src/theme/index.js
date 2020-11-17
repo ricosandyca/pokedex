eb009e859b9216ddfe5e29b30c964ec4771b70c6
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-17 14:51:04 +0700

    Change primary theme accent

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,4 +1,5 @@
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+import blue from '@material-ui/core/colors/blue'
 
 export default function initTheme() {
   const generateTextColor = (opacity = 1) => `rgba(80,80,80,${opacity})`
@@ -14,6 +15,12 @@ export default function initTheme() {
         secondary: generateTextColor(.7),
         disabled: generateTextColor(.5),
         hint: generateTextColor(.5)
+      },
+      primary: {
+        main: blue[500],
+        light: blue[300],
+        dark: blue[700],
+        contrastText: '#FFF'
       }
     }
   })
