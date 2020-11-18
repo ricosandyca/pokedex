6aaf5006f253d8d3c04b25015380e8e2c6011a76
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-18 22:47:23 +0700

    Change pokemon color extraction

--- src/utils/image-color-extractor.js
+++ src/utils/image-color-extractor.js
@@ -14,11 +14,11 @@ export default function extractImageColor(imgElm) {
   const extractedColors = colorThief.getColor(imgElm)
   const currentColor = color(extractedColors)
   const dominant = currentColor.darken(.1).hex()
-  const dominantLighten = currentColor.lighten(.1).hex()
+  const dominantLighten = currentColor.lighten(.15).hex()
 
   // get contrast text color
   const isLight = color(dominantLighten).darken(.25).isLight()
-  const contrastText = isLight ? '#000' : '#FFF'
+  const contrastText = isLight ? '#000000' : '#FFFFFF'
 
   return {
     dominant,
