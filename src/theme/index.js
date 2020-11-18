74f188df0975c14638dd0682d3225df9ebd750ca
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-18 23:42:07 +0700

    Change color theme

--- src/theme/index.js
+++ src/theme/index.js
@@ -10,7 +10,7 @@ export default function initTheme() {
   return createMuiTheme({
     palette: {
       background: {
-        default: '#ECF3FE',
+        default: '#FDFDFD',
         paper: '#FFFFFF'
       },
       text: {
