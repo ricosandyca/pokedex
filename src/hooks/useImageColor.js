f705c697b4b8bda9fd6ec18e47b7b02ca9557939
Rico Sandyca <ricosandyca.if@gmail.com>
2020-11-19 11:46:30 +0700

    Add useImageColor hooks info

--- src/hooks/useImageColor.js
+++ src/hooks/useImageColor.js
@@ -1,6 +1,13 @@
 import { useState, useRef, useEffect } from 'react'
 import extractImageColor from '../utils/image-color-extractor'
 
+/**
+ * Use image color hooks
+ * By creating image DOM and extracting the colors of it
+ * 
+ * @param {String} imagePath - Image path, can be extrenal or internal url
+ * @returns {Object} object of colors and image component
+ */
 export default function useImageColor(imagePath) {
   const [colors, setColors] = useState({})
   const [imageLoaded, setImageLoaded] = useState(false)
